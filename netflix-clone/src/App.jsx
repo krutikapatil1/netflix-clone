8dcf93c5c70b4ada8690222bd1b9de8647527504
Krutika Patil <krutiks89@gmail.com>
2021-06-13 23:40:24 -0400

    fix #2: Initialized axios and requests

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import axios from "./axios";
+import requests from "./requests";
 function App() {
-  return <div className="App"></div>;
+  const getNetflixOriginals = async () => {
+    const movies = await axios.get(requests.fetchNetflixOriginals);
+    console.table(movies.data);
+  };
+  getNetflixOriginals();
+  return (
+    <div className="App">
+      <h1>Let's build Netflix clone using react 🚀 </h1>
+    </div>
+  );
 }
 
 export default App;
